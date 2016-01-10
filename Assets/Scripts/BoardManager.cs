a307ee7b39fae4d110daf376699de3d389cba8a7
Jaehyun Jang <jaehyunjang@kaist.ac.kr>
2016-01-10 15:54:51 +0900

    Add a basic project structure

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace Roguelike
+{
+    public class BoardManager : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
